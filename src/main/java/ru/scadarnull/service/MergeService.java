e8a944c2613ee875011f3119bb676fe4bc7f6a66
Scadar <teghtml@yandex.ru>
2020-07-16 17:10:52 +0500

    update logic in leftJoin

--- src/main/java/ru/scadarnull/service/MergeService.java
+++ src/main/java/ru/scadarnull/service/MergeService.java
@@ -10,13 +10,12 @@ public class MergeService {
     public static List<Triple> leftJoin(ArrayList<Pair> table1, ArrayList<Pair> table2){
         //Надо именно через такие структуры данных или можно через интерфейсы?
         ArrayList<Triple> result = new ArrayList<>();
-        for(Pair line : table1){
-            result.add(new Triple(line.getId(), line.getValue(), ""));
-        }
-        for(Triple triple : result){
-            for(Pair line2 : table2){
-                if(triple.getId().equals(line2.getId())){
-                    triple.setValue2(line2.getValue());
+        for(Pair pair1 : table1){
+            Triple temp = new Triple(pair1.getId(), pair1.getValue(), "");
+            result.add(temp);
+            for(Pair pair2 : table2){
+                if(pair1.getId().equals(pair2.getId())){
+                    temp.setValue2(pair2.getValue());
                     break;
                 }
             }
@@ -25,21 +24,28 @@ public class MergeService {
     }
 
     public static List<Triple> leftJoin(LinkedList<Pair> table1, LinkedList<Pair> table2){
-        //Такую же логику можно и с ArrayList
+        //Такую же логику можно и с ArrayList(только надо отсортировать)
         List<Triple> result = new LinkedList<>();
         Iterator<Pair> iter = table2.listIterator();
-        Pair pair = table2.getFirst();//Обработать 1 элемент (его отсутствие)
-        for(Pair line : table1){
-            result.add(new Triple(line.getId(), line.getValue(), ""));
-        }
-        for(Triple triple : result){
-            while (iter.hasNext() && triple.getId().compareTo(pair.getId()) > 0){
-                pair = iter.next();
+        if(table2.size() == 0){
+            for(Pair pair : table1){
+                result.add(new Triple(pair.getId(), pair.getValue(), ""));
             }
-            if(triple.getId().compareTo(pair.getId()) == 0){
-                triple.setValue2(pair.getValue());
+            return result;
+        }
+        Pair firstPair = table2.getFirst();
+
+        for(Pair pair : table1){
+            while (iter.hasNext() && pair.getId().compareTo(firstPair.getId()) > 0){
+                firstPair = iter.next();
+            }
+            if(pair.getId().compareTo(firstPair.getId()) == 0){
+                result.add(new Triple(pair.getId(), pair.getValue(), firstPair.getValue()));
+            }else{
+                result.add(new Triple(pair.getId(), pair.getValue(), ""));
             }
         }
+
         return result;
     }
 
